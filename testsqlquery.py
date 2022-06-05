1a5f49cc2c3f745d7b5733eb8d53befe4624527c
@nekokat <nekokat89@gmail.com>
2022-06-05 21:23:38 +0300

    remade sql query

--- testsqlquery.py
+++ testsqlquery.py
@@ -2,7 +2,7 @@ from sql import Sql as Query
 
 r = Query()
 
-rows = ('Обычный', 'Нильфгаард', 'jardelxl', 'Скоя’таэли', 'Поражение', '2:2')
+rows = ('Обычный', 'Нильфгаард', 'jardelxl', 'Скоя’таэли', 'Поражение', '1:2')
 header = ("game_mode", "fraction", "opponent", "opponent_fraction", "result", "score")
 r.update("lastrow").set(score="1:2").where(rowid=1).execute()
 r.commit()
